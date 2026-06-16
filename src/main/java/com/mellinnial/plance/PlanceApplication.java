50d3ab56ff0b44722f57ad92a1d64ebafdc2f568
dhanjayarya <dhanjayarya20@gmail.com>
2026-06-16 22:44:08 +0530

    first commit with init setup

---
+++ src/main/java/com/mellinnial/plance/PlanceApplication.java
@@ -0,0 +1,13 @@
+package com.mellinnial.plance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mellinnial/plance/PlanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mellinnial.plance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
